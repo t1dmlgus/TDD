1da9b7bac26ec1316067696a245fceaf40b87855
t1dmlgus <dmlgusgngl@gmail.com>
2021-07-02 17:39:17 +0900

    refactor: 문자열 계산기

--- src/main/java/com/s1dmlgus/tdd/stringCalculator/StringCalculator.java
+++ src/main/java/com/s1dmlgus/tdd/stringCalculator/StringCalculator.java
@@ -1,9 +1,12 @@
 package com.s1dmlgus.tdd.stringCalculator;
 
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Formatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 /**
  * 리펙토링
@@ -11,15 +14,40 @@ import java.util.regex.Pattern;
  * 1. indent를 줄여라
  * 2. else 를 쓰지마라
  * 3. 메소드에 한가지 일만 하도록 하여라
+ *
+ *
+ * ### 기능 요구사항
+ *
+ * - 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한다, 각 숫자의 합을 반환한다.
+ *
+ *      1. null_빈칸
+ *          "" -> 0, null -> 0
+ *
+ *      2. 숫자 하나
+ *
+ *      3. 쉼표 하나
+ *
+ *      4. 쉼표 + 클론
+ *
+ *      5. 클론 하나
+ *
+ *
+ * - 앞의 기본 구분자(쉼표, 콜론)외에 커스텀 구분자를 지정할 수 있다.
+ *   커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다.
+ *
+ *      1. 커스텀 구분자 테스트
+ *
  * 
+ * - 문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw한다.
  * 
+ *      1. 음수 값 테스트 -> 예외처리
+ *
+ *
  */
-
 
 // 프로덕션 코드
 public class StringCalculator {
     public int add(String s) {
-
         if (isBlank(s)) {
             return 0;
         }
@@ -27,41 +55,38 @@ public class StringCalculator {
         return sum(toInt(split(s)));
     }
 
-    private int sum(int[] ints) {
-        int sum = 0;
-        for (int int01 : ints) {
-            sum += int01;
-        }
-        System.out.println("sum = " + sum);
-        return sum;
-    }
-
-    private String[] split(String s) {
-
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(s);
-        if (matcher.find()) {
-
-            return matcher.group(2).split(matcher.group(1));
-        }
-
-        return s.split(",|:");
-    }
-
-
-    private int[] toInt(String[] s1) {
-
-        return Arrays.stream(s1).mapToInt(Integer::parseInt).toArray();
-    }
-
 
     private boolean isBlank(String s) {
         return s == null || s.isEmpty();
     }
 
+    private String[] split(String s) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(s);
+        if(matcher.find()){
+            String[] split = matcher.group(2).split(matcher.group(1));
+            return split;
+        }
 
-    public static void main(String[] args) {
-        StringCalculator stringCalculator = new StringCalculator();
-        stringCalculator.add("3:6,4");
+
+        return s.split(",|:");
+    }
+
+    private int[] toInt(String[] split) {
+
+        int[] ints = Arrays.stream(split).mapToInt(Integer::parseInt).toArray();
+
+        for (int anInt : ints) {
+            if (anInt < 0) {
+                throw new RuntimeException("음수 값은 넣을 수 없습니다.");
+            }
+        }
+        return ints;
+    }
+
+    private int sum(int[] ints) {
+        return Arrays.stream(ints).sum();
     }
 
 }
+
+
--- src/test/java/com/s1dmlgus/tdd/stringCalculator/StringCalculatorTest.java
+++ src/test/java/com/s1dmlgus/tdd/stringCalculator/StringCalculatorTest.java
@@ -9,86 +9,106 @@ import static org.assertj.core.api.Assertions.*;
 
 
 /**
- *
- * 요구사항
- *
- * 1.쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분 리한 각 숫자의 합을 반환
- *  (예:
- *    “” => 0,
- *    "1,2" => 3,
- *    "1,2,3" => 6,
- *    “1,2:3” => 6)
- *
- * 2. 앞의 기본 구분자(쉼표, 콜론)외에 커스텀 구분자를 지정할 수 있다. 커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다. 예를 들어 “//;\n1;2;3”과 같이 값을 입력할 경우 커스텀 구분자는 세미콜론(;)이며, 결과 값 은 6이 반환되어야 한다.
-
- * 3. 문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외 를 throw한다.
+ * 리펙토링
+ * <p>
+ * 1. indent를 줄여라
+ * 2. else 를 쓰지마라
+ * 3. 메소드에 한가지 일만 하도록 하여라
  *
  *
  *
+ * ### 기능 요구사항
+ * - 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한다, 각 숫자의 합을 반환한다.
+ * <p>
+ * 1. null_빈칸
+ * <p>
+ * 2. 숫자 하나
+ * <p>
+ * 3. 쉼표 하나
+ * <p>
+ * 4. 쉼표 + 클론
+ * <p>
+ * 5. 클론 하나
+ * <p>
+ * <p>
+ * - 앞의 기본 구분자(쉼표, 콜론)외에 커스텀 구분자를 지정할 수 있다.
+ * 커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다.
  *
+ *
+ * 1. 커스텀 구분자 테스트
+ * <p>
+ * <p>
+ * - 문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw한다.
+ * <p>
+ * 1. 음수 값 테스트 -> 예외처리
  */
-
-
 
 
 class StringCalculatorTest {
 
-    private StringCalculator stringCalculator;
 
+    private StringCalculator stringCalculator;
 
     @BeforeEach
     void setUp() {
-
         stringCalculator = new StringCalculator();
     }
 
-
     @Test
-    public void null_또는_빈칸() throws Exception{
-        //given
-        //when
+    public void null_빈칸() throws Exception {
+
         //then
-        assertThat(stringCalculator.add("")).isEqualTo(0);
-        assertThat(stringCalculator.add(null)).isEqualTo(0);
+        Assertions.assertThat(stringCalculator.add("")).isEqualTo(0);
+        Assertions.assertThat(stringCalculator.add(null)).isEqualTo(0);
 
     }
 
     @Test
-    public void 값_하나() throws Exception{
-        //given
-        //when
+    public void 숫자하나() throws Exception{
+
         //then
         assertThat(stringCalculator.add("1")).isEqualTo(1);
-
     }
-
-    @Test
-    public void 쉼표() throws Exception{
-        //given
-        //when
-        //then
-
-        Assertions.assertThat(stringCalculator.add("1,2")).isEqualTo(3);
-    }
-
     
+    @Test
+    public void 쉼표하나() throws Exception{
+        //then
+        assertThat(stringCalculator.add("1,2")).isEqualTo(3);
+    }
+
+
     @Test
     public void 쉼표_또는_클론() throws Exception{
-        //given
-        //when
+
         //then
-        Assertions.assertThat(stringCalculator.add("1,2:4")).isEqualTo(7);
+        assertThat(stringCalculator.add("1,2:4")).isEqualTo(7);
     }
-    
-    
     @Test
-    public void 커스텀구분자() throws Exception{
-        //given
-        
-        //when
+    public void 클론하나() throws Exception{
         //then
-        Assertions.assertThat(stringCalculator.add("//;\n1;2;5")).isEqualTo(8);
+        assertThat(stringCalculator.add("1:2:5")).isEqualTo(8);
+    }
+
+
+    @Test
+    public void 커스텀문자열() throws Exception{
+
+        //then
+        assertThat(stringCalculator.add("//s\n2s5s7")).isEqualTo(14);
+    }
+
+
+    @Test
+    public void 음수_값_테스트() {
+
+        //then
+        assertThatThrownBy(()->{
+                    stringCalculator.add("-1:2,3");
+                })
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("음수 값은 넣을 수 없습니다.");
     }
     
+
 
 }
